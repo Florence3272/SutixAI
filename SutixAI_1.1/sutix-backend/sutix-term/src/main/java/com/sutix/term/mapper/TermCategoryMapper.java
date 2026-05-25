93926424865a02f70d4b28d2c935f7512c3d4d5a
Fragileshs <145463763+Fragileshs@users.noreply.github.com>
2026-05-25 15:11:04 +0800

    Delete SutixAI_1.1 directory

--- SutixAI_1.1/sutix-backend/sutix-term/src/main/java/com/sutix/term/mapper/TermCategoryMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sutix.term.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sutix.term.entity.TermCategory;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TermCategoryMapper extends BaseMapper<TermCategory> {
-}
