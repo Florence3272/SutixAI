6650e3aa0fa6bbc425197c97d2f20c5c3c345ba1
Fragileshs <145463763+Fragileshs@users.noreply.github.com>
2026-05-25 15:11:30 +0800

    Delete SutixAI_1.0/SutixAI-main directory

--- SutixAI_1.0/SutixAI-main/sutix-backend/sutix-system/src/main/java/com/sutix/system/mapper/SysTermMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sutix.system.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sutix.system.entity.SysTerm;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface SysTermMapper extends BaseMapper<SysTerm> {
-}
