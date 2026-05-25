40eb3075b8cbf245b38683576467c98636dc99be
刘伊鸣 <17356563272@163.com>
2026-05-25 16:16:00 +0800

    Update index.js

--- SutixAI_1.0/sutix-front/sutix-front/src/router/index.js
+++ SutixAI_1.0/sutix-front/sutix-front/src/router/index.js
@@ -1,10 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+import UserLayout from '@/layouts/UserLayout.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/login', component: Login }
+  {
+    path: '/',
+    component: UserLayout,
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/pages/user/Home.vue')
+      },
+      {
+        path: 'login',
+        name: 'Login',
+        component: () => import('@/pages/user/Login.vue')
+      },
+      {
+        path: 'term-list',
+        name: 'TermList',
+        component: () => import('@/pages/user/TermList.vue')
+      },
+      {
+        // 修复点1：子路由不能以/开头
+        path: 'category/:categoryId', 
+        name: 'CategoryDatabase',
+        // 修复点2：添加懒加载导入（与其他路由保持一致）
+        component: () => import('@/pages/user/CategoryDatabase.vue')
+      },
+      {
+        path: 'term-detail',
+        name: 'TermDetail',
+        component: () => import('@/pages/user/TermDetail.vue')
+      },
+      {
+        path: 'term-study',
+        name: 'TermStudy',
+        component: () => import('@/pages/user/TermStudy.vue')
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
