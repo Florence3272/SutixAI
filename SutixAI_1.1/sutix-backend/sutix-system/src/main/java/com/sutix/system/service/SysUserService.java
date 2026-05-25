66ecbfd1dab61c5a852d5fc3914bc5421190d36b
刘伊鸣 <17356563272@163.com>
2026-05-25 16:05:23 +0800

    feat: add sutix-front-1.1 version

--- SutixAI_1.1/sutix-backend/sutix-system/src/main/java/com/sutix/system/service/SysUserService.java
+++
@@ -1,8 +0,0 @@
-package com.sutix.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.sutix.system.entity.SysUser;
-
-public interface SysUserService extends IService<SysUser> {
-    SysUser getByUsername(String username);
-}
